ea30cfeaa3d3e558bdc7429dec0557f1d2abb353
Vinit Vijan <vinit26virgo@gmail.com>
2019-08-05 11:58:01 +0530

    Modify AI to follow row column over x,y coords for remaining ones

--- server/src/AI.js
+++ server/src/AI.js
@@ -5,8 +5,8 @@ let Tile = require( './Tile.js' );
 
 let AI = function () {
 
-    this.aiTargetX = 0; //global variables
-    this.aiTargetY = 0;
+    this.aiTargetRow = 0; //global variables
+    this.aiTargetCol = 0;
     this.flagTarget = false;
     this.game = null;
     this.targetsList = []; //has a list of tiles on which the AI will perform either click or flag action
@@ -21,14 +21,14 @@ let AI = function () {
     this.aiSolve = function () {
         let newBoardConfig = null;
         if ( !flagTarget ) { // Make a move
-            if ( this.game.board.grid[ this.aiTargetX ][ this.aiTargetY ].isFlagged == true ) {
+            if ( this.game.board.grid[ this.aiTargetRow ][ this.aiTargetCol ].isFlagged == true ) {
                 console.log( "Incorrect click attempt." );
             } else {
-                newBoardConfig = this.game.handleClick( this.aiTargetX, this.aiTargetY );
+                newBoardConfig = this.game.handleClick( this.aiTargetRow, this.aiTargetCol );
             }
         } else {
-            if ( this.game.board.grid[ this.aiTargetX ][ this.aiTargetY ].isFlagged == false ) {
-                newBoardConfig = this.game.handleFlag( this.aiTargetX, this.aiTargetY );
+            if ( this.game.board.grid[ this.aiTargetRow ][ this.aiTargetCol ].isFlagged == false ) {
+                newBoardConfig = this.game.handleFlag( this.aiTargetRow, this.aiTargetCol );
             }
         }
 
@@ -38,7 +38,7 @@ let AI = function () {
         // Remove any tiles which are present in targetsList if they have been revealed
         // This will save a click
         for ( var i = 0; i < this.targetsList.length; i++ ) {
-            if ( this.game.board.grid[ this.targetsList[ i ].x ][ this.targetsList[ i ].y ].hidden == false ) {
+            if ( this.game.board.grid[ this.targetsList[ i ].row ][ this.targetsList[ i ].col ].hidden == false ) {
                 this.targetsList.splice( i );
                 i--;
             }
@@ -46,8 +46,8 @@ let AI = function () {
 
         // Get the next target from the targetsList
         if ( this.targetsList.length !== 0 ) {
-            this.aiTargetX = this.targetsList[ 0 ].x;
-            this.aiTargetY = this.targetsList[ 0 ].y;
+            this.aiTargetRow = this.targetsList[ 0 ].row;
+            this.aiTargetCol = this.targetsList[ 0 ].col;
             if ( targetsList[ 0 ].needToFlag ) {
                 this.flagTarget = true;
                 console.log( "targetList flags" );
@@ -222,7 +222,7 @@ let AI = function () {
                                         for ( var m = 0; m < this.game.board.grid[ row ][ col ].allHiddenNeighbours.length; m++ ) {
                                             if ( !linkedTilesAdjacentToThis.includes( this.game.board.grid[ row ][ col ].allHiddenNeighbours[ m ] ) ) {
                                                 temp = this.game.board.grid[ row ][ col ].allHiddenNeighbours[ m ];
-                                                this.targetsList.push( new Target( temp.x, temp.y, true ) );
+                                                this.targetsList.push( new Target( temp.row, temp.col, true ) );
                                                 console.log( "aiFindTargetsUsingLinkedInfo clicks" );
                                                 temp.alreadyTargeted = true;
                                             }
@@ -247,20 +247,20 @@ let AI = function () {
             if ( this.aiCorners[ cornerId ] == 0 ) {
                 switch ( cornerId ) {
                     case 0:
-                        this.aiTargetX = 0;
-                        this.aiTargetY = 0;
+                        this.aiTargetRow = 0;
+                        this.aiTargetCol = 0;
                         break;
                     case 1:
-                        this.aiTargetX = 0;
-                        this.aiTargetY = this.game.board.cols - 1;
+                        this.aiTargetRow = 0;
+                        this.aiTargetCol = this.game.board.cols - 1;
                         break;
                     case 2:
-                        this.aiTargetX = this.game.board.rows - 1;
-                        this.aiTargetY = 0;
+                        this.aiTargetRow = this.game.board.rows - 1;
+                        this.aiTargetCol = 0;
                         break;
                     case 3:
-                        this.aiTargetX = this.game.board.rows - 1;
-                        this.aiTargetY = this.game.board.cols - 1;
+                        this.aiTargetRow = this.game.board.rows - 1;
+                        this.aiTargetCol = this.game.board.cols - 1;
                         break;
                 }
                 this.aiCorners[ cornerId ] = 1;
@@ -269,18 +269,18 @@ let AI = function () {
             }
         } else {
             do {
-                aiTargetX = Math.floor( Math.random() * this.game.board.rows );
-                aiTargetY = Math.floor( Math.random() * this.game.board.cols );
-            } while ( this.game.board.grid[ aiTargetX ][ aiTargetY ].hidden == false ||
-                this.game.board.grid[ aiTargetX ][ aiTargetY ].isFlagged == true || this.game.board.grid[ aiTargetX ][ aiTargetY ].hasMine == true );
+                aiTargetRow = Math.floor( Math.random() * this.game.board.rows );
+                aiTargetCol = Math.floor( Math.random() * this.game.board.cols );
+            } while ( this.game.board.grid[ aiTargetRow ][ aiTargetCol ].hidden == false ||
+                this.game.board.grid[ aiTargetRow ][ aiTargetCol ].isFlagged == true || this.game.board.grid[ aiTargetRow ][ aiTargetCol ].hasMine == true );
             this.flagTarget = false;
         }
     };
 
     //stores the x-y coordinates and the action to be performed on it
-    this.Target( x, y, needToFlag ) = function () { //DOUBT - is this correct?
-        this.x = x;
-        this.y = y;
+    this.Target( row, col, needToFlag ) = function () { //DOUBT - is this correct?
+        this.row = row;
+        this.col = col;
         this.needToFlag = needToFlag;
     };
 
